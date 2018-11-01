e71578ff619fc67769f87a55a5b0ab1608c31448
Unknown <velimatti92@hotmail.com>
2018-11-01 11:43:08 +0200

    voimaa pallolle

--- UnityTest/Assets/Scripts/NewBehaviourScript.cs
+++ UnityTest/Assets/Scripts/NewBehaviourScript.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
 }
+
